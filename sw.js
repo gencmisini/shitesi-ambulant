e7a87251864749fdee26ecaba8090dfcac05441f
Genc Misini <35168316+gencmisini@users.noreply.github.com>
2026-05-13 00:26:13 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'besi-godanc-v1';
-const FILES = ['./', './shitesi-ambulant.html', './manifest.json', './icon-192.png', './icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{}))); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
